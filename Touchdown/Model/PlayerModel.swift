faf4a577930d0322afbb40441cd87a318528d2f0
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-23 11:42:58 -0300

    Build slider menu in home view

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Samuel Brehm on 23/04/21.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
