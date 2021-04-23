10bc1972b8c8cb4cf01713f87b14b84cae30f7d8
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-23 08:55:58 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Samuel Brehm on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
