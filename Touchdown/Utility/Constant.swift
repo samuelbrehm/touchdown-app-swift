8d488fb777bd876fba2ea4fbeabd926dba441d3f
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-23 09:51:13 -0300

    Added file contants and footer view

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Samuel Brehm on 23/04/21.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
